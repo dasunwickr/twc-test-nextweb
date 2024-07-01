ae3e72a391ea71cf669b2db4e1514bf1562f8df6
Thathsara Dasun Wickramasooriya <32405325+dasunwickr@users.noreply.github.com>
2024-07-01 17:12:53 +0530

    Contacts Table UI

--- src/app/contact/layout.tsx
+++ src/app/contact/layout.tsx
@@ -3,14 +3,14 @@ import Image from "next/image"
 
 const ContactsPage = ({ children }: LayoutPageProps) => {
     return (
-        <div className=" h-screen px-[204px] bg-ellipse-1 flex flex-col justify-evenly">
-            <div className="flex flex-col ">
+        <div className=" h-screen py-16 px-[204px] bg-ellipse-1 flex flex-col justify-evenly">
+            <div className="flex flex-col flex-1 ">
                 <Image src='/logo_white.png' width={72.94} height={24.03} alt='logo' />
                 <span className="text-3xl font-bold">contacts</span>
                 <span className="text-3xl font-normal">portal</span>
             </div>
-            <div>{children}</div>
-            <div className="flex items-center gap-4 ml-auto -mr-8">
+            <div className="flex-auto">{children}</div>
+            <div className=" flex-1 flex items-center gap-4 ml-auto  -mr-8">
                 <Image src='/logout_btn.png' width={43} height={43} alt='logout' />
                 <button className="text-xl underline">logout</button>
             </div>
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,104 @@
+import Image from "next/image";
+import { Icon } from "@iconify/react";
+
 const ContactPage = () => {
     return (
-        <div className="bg-black">Test</div>
-    )
-}
+        <div className="h-full my-8">
+            <div className="flex justify-between items-center">
+                <div className="text-4xl italic font-normal">Contacts</div>
+                <div>
+                    <button className="border-2 text-xl rounded-full px-8 py-2">Add New Contact</button>
+                </div>
+            </div>
+            <div className="bg-white rounded-2xl mt-8 pt-4 h-72 text-green-700 p-2">
+                <div className="max-h-64 overflow-y-auto">
+                    <table className="w-full">
+                        <thead className="sticky top-0 bg-white">
+                            <tr>
+                                <th className="text-left"></th>
+                                <th className="text-left">Name</th>
+                                <th className="text-left">Email</th>
+                                <th className="text-left">Phone</th>
+                                <th className="text-left"></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td className="flex justify-center items-center">
+                                    <Image src="/female.png" width={50} height={50} alt="avatar" />
+                                </td>
+                                <td className="text-left">John Doe</td>
+                                <td className="text-left">john@example.com</td>
+                                <td className="text-left">(123) 456-7890</td>
+                                <td>
+                                    <div className="flex justify-end p-2 gap-2">
+                                        <Icon icon="mdi:trash-can-outline" width="24" height="24" />
+                                        <Icon icon="mdi:pencil-outline" width="24" height="24" />
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td className="flex justify-center items-center">
+                                    <Image src="/female.png" width={50} height={50} alt="avatar" />
+                                </td>
+                                <td className="text-left">John Doe</td>
+                                <td className="text-left">john@example.com</td>
+                                <td className="text-left">(123) 456-7890</td>
+                                <td>
+                                    <div className="flex justify-end p-2 gap-2">
+                                        <Icon icon="mdi:trash-can-outline" width="24" height="24" />
+                                        <Icon icon="mdi:pencil-outline" width="24" height="24" />
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td className="flex justify-center items-center">
+                                    <Image src="/female.png" width={50} height={50} alt="avatar" />
+                                </td>
+                                <td className="text-left">Jane Smith</td>
+                                <td className="text-left">jane@example.com</td>
+                                <td className="text-left">(987) 654-3210</td>
+                                <td>
+                                    <div className="flex justify-end p-2 gap-2">
+                                        <Icon icon="mdi:trash-can-outline" width="24" height="24" />
+                                        <Icon icon="mdi:pencil-outline" width="24" height="24" />
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td className="flex justify-center items-center">
+                                    <Image src="/female.png" width={50} height={50} alt="avatar" />
+                                </td>
+                                <td className="text-left">Jane Smith</td>
+                                <td className="text-left">jane@example.com</td>
+                                <td className="text-left">(987) 654-3210</td>
+                                <td>
+                                    <div className="flex justify-end p-2 gap-2">
+                                        <Icon icon="mdi:trash-can-outline" width="24" height="24" />
+                                        <Icon icon="mdi:pencil-outline" width="24" height="24" />
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td className="flex justify-center items-center">
+                                    <Image src="/female.png" width={50} height={50} alt="avatar" />
+                                </td>
+                                <td className="text-left">Jane Smith</td>
+                                <td className="text-left">jane@example.com</td>
+                                <td className="text-left">(987) 654-3210</td>
+                                <td>
+                                    <div className="flex justify-end p-2 gap-2">
+                                        <Icon icon="mdi:trash-can-outline" width="24" height="24" />
+                                        <Icon icon="mdi:pencil-outline" width="24" height="24" />
+                                    </div>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    );
+};
 
-export default ContactPage
+export default ContactPage;
