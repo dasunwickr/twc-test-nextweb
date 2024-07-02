641b1e8e513ed9b12f511c488ebef35eedeac899
Thathsara Dasun Wickramasooriya <32405325+dasunwickr@users.noreply.github.com>
2024-07-02 09:56:23 +0530

    tailwind font fix

--- src/app/contact/layout.tsx
+++ src/app/contact/layout.tsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 
 const ContactsPage = ({ children }: LayoutPageProps) => {
     return (
-        <div className=" h-screen py-16 px-[204px] bg-ellipse-1 flex flex-col justify-evenly">
+        <div className=" h-screen py-16 px-[204px] bg-ellipse-1 bg-cover flex flex-col justify-evenly">
             <div className="flex flex-col flex-1 ">
                 <Image src='/logo_white.png' width={72.94} height={24.03} alt='logo' />
                 <span className="text-3xl font-bold">contacts</span>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image"
 
 const WelcomePage = () => {
   return (
-    <div className=" h-screen px-[204px] bg-ellipse-1 flex flex-col justify-evenly">
+    <div className=" h-screen px-[204px] bg-ellipse-1 bg-cover flex flex-col justify-evenly">
       <div className="flex flex-col ">
         <Image src='/logo_white.png' width={72.94} height={24.03} alt='logo' />
         <span className="text-3xl font-bold">contacts</span>
